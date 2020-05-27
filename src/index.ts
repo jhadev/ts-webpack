9634bd9ad3f6e3df97d64ae2c88af4a1df3e371f
jhadev <joshappeldev@gmail.com>
2020-05-27 15:46:39 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,59 @@
-let arr: [string, string, number, number] = ['hi', 'cool', 5, 9];
-
 const section = document.getElementsByTagName('section')[0];
 
-const p = document.createElement('p');
+interface Person {
+  firstName: string;
+  lastName?: string;
+  position?: string;
+  fullName?: string;
+}
 
-p.textContent = `${arr.join(', ')}`;
+function createPerson(
+  person: Person
+): {
+  firstName: string;
+  lastName?: string;
+  fullName?: string;
+  position?: string;
+} {
+  if (person.lastName) {
+    person.fullName = `${person.firstName} ${person.lastName}`;
+  }
 
-section?.appendChild(p);
+  return person;
+}
+
+const me: Person = createPerson({
+  firstName: 'Josh',
+  lastName: 'A',
+  position: 'Developer',
+});
+
+const somebody: Person = createPerson({
+  firstName: 'Some',
+  lastName: 'Body',
+});
+
+function filterPeople(people: Person[], value: string): Person[] | [] {
+  return people.filter((person: Person) => person.firstName !== value);
+}
+
+let filtered: Person[] = filterPeople([me, somebody], 'Some');
+
+function print(arr: any[]) {
+  arr.forEach((item) => {
+    const wrapper = document.createElement('div');
+    section?.appendChild(wrapper);
+
+    const ul = document.createElement('ul');
+
+    for (let [key, value] of Object.entries(item)) {
+      const li = document.createElement('li');
+      li.innerHTML = `${key}: ${value}`;
+      ul?.appendChild(li);
+    }
+
+    wrapper?.appendChild(ul);
+  });
+}
+
+print(filtered);
